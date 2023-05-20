bb6fb219da1bf7ab66e2c5468632c261129cc5cf
@jc-nieto <jc.nieto@uniandes.edu.co>
2023-05-20 18:54:28 -0500

    Adicion prueba modificar stock

--- tests/ccp.spec.ts
+++ tests/ccp.spec.ts
@@ -6,6 +6,7 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Pruebas E2E para login', () => {
     test('Realizar login con user1', async ({ page }) => {
+      test.slow();
       // create a new user locator
       const newUser = page.getByPlaceholder('Escriba su usuario');
   
@@ -34,6 +35,7 @@ test.describe('Pruebas E2E para login', () => {
       await expect(newUser).toHaveText('');      
     });
     test('Realizar login con usuario desconocido', async ({ page }) => {
+      test.slow();
       // create a new user locator
       const newUser = page.getByPlaceholder('Escriba su usuario');
   
@@ -50,12 +52,14 @@ test.describe('Pruebas E2E para login', () => {
   
       // Click the get started link.
       await page.getByText('Ingresar').click();
+      await page.waitForTimeout(3000);
   
-      // Expects the URL to contain intro.
+      // Expects the message.
       const newMessage = await page.getByText('El usuario no fue encontrado.');
       await expect(newMessage).toBeVisible();
     });
     test('Realizar login con password inválido', async ({ page }) => {
+      test.slow();
       // create a new user locator
       const newUser = page.getByPlaceholder('Escriba su usuario');
   
@@ -72,7 +76,8 @@ test.describe('Pruebas E2E para login', () => {
   
       // Click the get started link.
       await page.getByText('Ingresar').click();
-  
+      await page.waitForTimeout(3000);
+
       // Expects the URL to contain intro.
       const newMessage = await page.getByText('No está autorizado para realizar ésta acción');
       await expect(newMessage).toBeVisible();
@@ -81,6 +86,7 @@ test.describe('Pruebas E2E para login', () => {
 
 test.describe('Pruebas E2E para productos', () => {
     test('Buscar y ver detalle de un producto existente', async ({ page }) => {
+      test.slow();
       // create a new user locator
       const newUser = page.getByPlaceholder('Escriba su usuario');
       // Fill a user.
@@ -94,15 +100,13 @@ test.describe('Pruebas E2E para productos', () => {
       // Click the get started link.
       await page.getByText('Ingresar').click();
 
-      // Expects the URL to contain dashboard
-      await expect(page).toHaveURL(/.*dashboard/);
-
       // Click the tab.
       await page.locator('div').filter({ hasText: /^Ajustes$/ }).first().click();   
       // Click the get started link.   
       await page.getByRole('link', { name: 'Productos' }).click();
       await page.getByRole('button', { name: 'Rows per page: 5' }).click();
       await page.getByRole('option', { name: '25' }).click();
+      await page.waitForTimeout(3000);
       await page.getByText('Nombre').click();
       await page.getByText('00AA').click();
 
@@ -121,59 +125,8 @@ test.describe('Pruebas E2E para productos', () => {
       await expect(newUser).toHaveText('');      
     });
 
-    // test('Consultar y modificar stock de un producto existente', async ({ page }) => {
-    //   // create a new user locator
-    //   const newUser = page.getByPlaceholder('Escriba su usuario');
-    //   // Fill a user.
-    //   await newUser.fill('user1');
-    //   await newUser.press('Enter');
-    //   // create a new password locator
-    //   const newPass = page.getByPlaceholder('Escriba su contraseña');
-    //   // Fill password  
-    //   await newPass.fill('password1');
-    //   await newPass.press('Enter');
-    //   // Click the get started link.
-    //   await page.getByText('Ingresar').click();
-
-    //   // Expects the URL to contain dashboard
-    //   await expect(page).toHaveURL(/.*dashboard/);
-
-    //   // Click the tab.
-    //   await page.locator('div').filter({ hasText: /^Ajustes$/ }).first().click();   
-    //   // Click the get started link.   
-    //   await page.getByRole('link', { name: 'Productos' }).click();
-    //   await page.getByRole('button', { name: 'Rows per page: 5' }).click();
-    //   await page.getByRole('option', { name: '25' }).click();
-    //   await page.getByText('Nombre').click();
-    //   const rowProduct = page.getByRole('row', { name: 'Select row product 00AA950aa256-1953-4b37-a693-05e432bbbb52 Otros 0 Activo' })
-      
-    //   await expect(rowProduct).toBeDefined();
-
-    //   //await rowProduct.click();
-    //   await rowProduct.getByTestId('CheckBoxIcon').check();
-
-    //   const actualStock = page.getByLabel('Current Stock');
-    //   const actualStockValue = actualStock.getAttribute('value')
-    //   console.log(actualStockValue);
-
-    //   await page.getByRole('button', { name: 'Actualizar' }).click();
-
-
-
-    //   // Click the tab.
-    //   await page.locator('div').filter({ hasText: /^Ajustes$/ }).first().click();   
-    //   // Click the get started link.
-    //   await page.getByText('Ajustes').click();
-    //   // Click the get started link.
-    //   await page.getByText('Salir').click();
-
-    //   // Expects to be in login menu without user
-    //   await expect(newUser).toHaveText('');   
-    // });
-  });
-
-  test.describe('Pruebas E2E para clientes', () => {
-    test('Buscar y ver detalle de un cliente existente', async ({ page }) => {
+    test('Consultar y modificar stock de un producto existente', async ({ page }) => {
+      test.slow();
       // create a new user locator
       const newUser = page.getByPlaceholder('Escriba su usuario');
       // Fill a user.
@@ -187,8 +140,59 @@ test.describe('Pruebas E2E para productos', () => {
       // Click the get started link.
       await page.getByText('Ingresar').click();
 
-      // Expects the URL to contain dashboard
-      await expect(page).toHaveURL(/.*dashboard/);
+      // Click the tab.
+      await page.locator('div').filter({ hasText: /^Ajustes$/ }).first().click();   
+      // Click the get started link.   
+      await page.getByRole('link', { name: 'Productos' }).click();
+      await page.getByText('Nombre').click();
+      await page.getByRole('button', { name: 'Menu' }).click();
+      await page.getByText('Filter').click();
+      await page.getByPlaceholder('Filter value').fill('00AA');
+      await page.waitForTimeout(3000);
+      await page.getByPlaceholder('Filter value').press('Enter');
+      await page.getByRole('button', { name: 'Rows per page: 5' }).click();
+      await page.getByRole('option', { name: '5', exact: true }).click();
+      await page.getByRole('checkbox', { name: 'Select row' }).check();
+      await page.locator('div').filter({ hasText: /^Todos los Productos$/ }).getByRole('button').first().click();
+
+      // Add 1 unit.
+      await page.getByPlaceholder('10').click();
+      await page.getByPlaceholder('10').fill('1');
+      await page.getByRole('button', { name: 'Actualizar' }).click();
+      await page.waitForTimeout(3000);
+
+      // Expects the message.
+      const newMessage = page.getByText('Proceso exitoso');
+      await expect(newMessage).toBeVisible();
+      await page.waitForTimeout(3000);
+
+      // Click the tab.
+      await page.locator('div').filter({ hasText: /^Ajustes$/ }).first().click();   
+      // Click the get started link.
+      await page.getByText('Ajustes').click();
+      // Click the get started link.
+      await page.getByText('Salir').click();
+
+      // Expects to be in login menu without user
+      await expect(newUser).toHaveText('');   
+    });
+  });
+
+  test.describe('Pruebas E2E para clientes', () => {
+    test('Buscar y ver detalle de un cliente existente', async ({ page }) => {
+      test.slow();
+      // create a new user locator
+      const newUser = page.getByPlaceholder('Escriba su usuario');
+      // Fill a user.
+      await newUser.fill('user1');
+      await newUser.press('Enter');
+      // create a new password locator
+      const newPass = page.getByPlaceholder('Escriba su contraseña');
+      // Fill password  
+      await newPass.fill('password1');
+      await newPass.press('Enter');
+      // Click the get started link.
+      await page.getByText('Ingresar').click();
 
       // Click the tab.
       await page.locator('div').filter({ hasText: /^Ajustes$/ }).first().click();   
@@ -196,6 +200,7 @@ test.describe('Pruebas E2E para productos', () => {
       await page.getByRole('link', { name: 'Clientes' }).click();
       await page.getByRole('button', { name: 'Rows per page: 5' }).click();
       await page.getByRole('option', { name: '25' }).click();
+      await page.waitForTimeout(3000);
       await page.getByText('Nombre').click();
       await page.getByText('Acme').click();
 
@@ -218,6 +223,7 @@ test.describe('Pruebas E2E para productos', () => {
 
   test.describe('Pruebas E2E para visitas', () => {
     test('Buscar y ver detalle de una visita existente', async ({ page }) => {
+      test.slow();
       // create a new user locator
       const newUser = page.getByPlaceholder('Escriba su usuario');
       // Fill a user.
@@ -231,16 +237,21 @@ test.describe('Pruebas E2E para productos', () => {
       // Click the get started link.
       await page.getByText('Ingresar').click();
 
-      // Expects the URL to contain dashboard
-      await expect(page).toHaveURL(/.*dashboard/);
-
       // Click the tab.
       await page.locator('div').filter({ hasText: /^Ajustes$/ }).first().click();   
       // Click the get started link.   
       await page.getByRole('link', { name: 'Visitas' }).click();
-      await page.getByRole('button', { name: 'Rows per page: 5' }).click();
-      await page.getByRole('option', { name: '25' }).click();
+      await page.waitForTimeout(8000);
       await page.getByText('Cliente', {exact: true}).click();
+
+      await page.getByRole('button', { name: 'Menu' }).click();
+      await page.getByText('Filter').click();
+      await page.getByPlaceholder('Filter value').fill('Acme');
+      await page.waitForTimeout(3000);
+      await page.getByPlaceholder('Filter value').press('Enter');
+      await page.getByRole('button', { name: 'Rows per page: 5' }).click();
+      await page.getByRole('option', { name: '5', exact: true }).click();
+
       await page.getByText('Acme').click();
 
       // Expect view detail for product
